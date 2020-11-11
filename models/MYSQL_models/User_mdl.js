b576fd3a1e2129727196d7e6715d73e690e8dc45
Dnoel26 <darnellnoel.webdev@gmail.com>
2020-11-11 18:20:08 +0000

    db test 14

--- models/MYSQL_models/User_mdl.js
+++ models/MYSQL_models/User_mdl.js
@@ -193,13 +193,10 @@ const User_model = {
             LEFT JOIN customer c ON u.user_id = c.customer_id_fk LEFT JOIN inventory_clerk ic ON u.user_id = ic.inventory_clerk_id_fk
             WHERE email = ? OR username = ?;`;
 
-            if(!MySQL_DB.connection)
-            {
-                console.log("CONNECTION DEAD",MySQL_DB.connection)
-                MySQL_DB.destroy()
-                .then(()=>{MySQL_DB.init();})
-                .catch((err)=>console.log(`Error in destroying DB`,err));
-            }
+            
+
+            console.log("CONNECTION DEAD",MySQL_DB.connection);
+            MySQL_DB.destroy();
 
             db.connection.query(this.SQL, [user_email,user_username]) //customer_email,customer_username])
             .then(([rows,fields])=>{
